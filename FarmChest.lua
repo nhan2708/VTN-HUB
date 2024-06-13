730d48172d5ef2af8c183aac8c41b3552f7b7148
nhan2708 <163325777+nhan2708@users.noreply.github.com>
2024-06-13 10:15:31 +0700

    FarmChestv5.lua

--- FarmChest.lua
+++ FarmChest.lua
@@ -69,7 +69,7 @@ function Teleport()
         end)
     end
 end
-local veryImportantWaitTime = 0.7
+local veryImportantWaitTime = 0.75
 task.spawn(function()
     while task.wait(veryImportantWaitTime) do
         pcall(function()
